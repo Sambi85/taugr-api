2e312ff6b7b50e6baa93fd9ac30f006b53e4d95a
CodyC <codyicastro@gmail.com>
2020-09-08 17:01:16 -0400

    Selected a different size photo from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ require 'pexels'
 client = Pexels::Client.new(ENV['PEXELS_API_KEY'])
 
 client.photos.search('Halloween Costume', per_page: 75).photos.each do |photo|
-    costume = Costume.create(title:Faker::Quotes::Shakespeare.hamlet_quote, url: photo.src.values[0], description: "Photographer: #{photo.user.name}")
+    costume = Costume.create(title:Faker::Quotes::Shakespeare.hamlet_quote, url: photo.src.values[2], description: "Photographer: #{photo.user.name}")
 end
 # Lets test if photo.src can return something with photo.src.original. We need it to be a single string not a weird funky hash of strings
 
