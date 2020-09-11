aa8a9b92772f0ade5b9917ea2be4943404bd5b82
github username <github email address>
2020-09-11 11:58:42 -0400

    fixed create method in costumes

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -8,5 +8,19 @@ class CostumesController < ApplicationController
         costume = Costume.find(params[:id])
         render json: costume
       end
+
+      def new
+        costume = Costume.new
+      end
+
+      def create
+        costume = Costume.create(costume_params)
+        render json: costume
+      end
       
+      private
+
+      def costume_params
+        params.require(:costume).permit(:title, :url, :description)
+      end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,12 @@ require 'byebug'
 require 'faker'
 require 'pexels'
 
-Costume.destroy_all
-
-
 client = Pexels::Client.new(ENV['PEXELS_API_KEY'])
 
 client.photos.search('Halloween Costume', per_page: 25).photos.each do |photo|
     costume = Costume.create(title:Faker::Quotes::Shakespeare.hamlet_quote, url: photo.src.values[2], description: "Photographer: #{photo.user.name}")
 end
 # Lets test if photo.src can return something with photo.src.original. We need it to be a single string not a weird funky hash of strings
-
-
-
 
 5.times do
     User.create(name:Faker::FunnyName.two_word_name)
