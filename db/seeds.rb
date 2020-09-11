9f64fe7ae204a0bb405efc3bab0401acef8ade0d
CodyC <codyicastro@gmail.com>
2020-09-10 22:46:24 -0400

    edited seeds to 25 to test easier

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ Costume.destroy_all
 
 client = Pexels::Client.new(ENV['PEXELS_API_KEY'])
 
-client.photos.search('Halloween Costume', per_page: 75).photos.each do |photo|
+client.photos.search('Halloween Costume', per_page: 25).photos.each do |photo|
     costume = Costume.create(title:Faker::Quotes::Shakespeare.hamlet_quote, url: photo.src.values[2], description: "Photographer: #{photo.user.name}")
 end
 # Lets test if photo.src can return something with photo.src.original. We need it to be a single string not a weird funky hash of strings
